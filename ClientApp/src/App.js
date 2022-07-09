7988e8fb44ded5a984f6ba1253cc02aa7f64e62c
cw-nikhil <nikhilrathore794@gmail.com>
2022-07-09 18:30:58 +0530

    initial setup

---
+++ ClientApp/src/App.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+import { Route } from 'react-router';
+
+
+export default class App extends Component {
+  static displayName = App.name;
+
+  render() {
+    return null;
+  }
+}
